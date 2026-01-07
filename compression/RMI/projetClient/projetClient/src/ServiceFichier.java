d099403685f04011cf134e887076be6a8e8ed126
Laytongentleman <lnmt21@protonmail.com>
2026-01-07 20:29:33 +0100

    restructuration

--- compression/RMI/projetClient/projetClient/src/ServiceFichier.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServiceFichier extends Remote {
-
-    void envoyerFichier(String nomFichier, byte[] contenu)
-            throws RemoteException;
-}
